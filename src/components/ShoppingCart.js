cf296e33f1ca2d51144bdcc41c30dfcba1b8c0dd
jacobjolibois <jacobjolibois@hey.com>
2022-01-20 14:28:03 -0600

    updates to README checklist

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -22,7 +22,7 @@ const ShoppingCart = () => {
 			))}
 
 			<div className="shopping-cart__checkout">
-				<p>Total: ${getCartTotal()}</p>
+				<p>{`Total: ${getCartTotal()}`}</p>
 				<button>Checkout</button>
 			</div>
 		</div>
